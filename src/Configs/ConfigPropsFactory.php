fb8be0162df626a5dbaafa3bb9d5af7371c9b02b
Daniel Ronkainen <daniel.ronkainen@wazabii.se>
2026-03-10 21:11:22 +0100

    chore: rename ConfigProps to CliOptions

--- src/Configs/ConfigPropsFactory.php
+++ src/Configs/ConfigPropsFactory.php
@@ -9,15 +9,16 @@ use MaplePHP\Emitron\Contracts\ConfigPropsInterface;
 
 class ConfigPropsFactory
 {
-    /**
-     * Get expected instance of Config Props
-     *
-     * @param array $props
-     * @return ConfigPropsInterface
-     */
+	/**
+	 * Get expected instance of Config Props
+	 *
+	 * @param array $props
+	 * @param string|null $configProps
+	 * @return ConfigPropsInterface
+	 */
     public static function create(array $props, ?string $configProps = null): ConfigPropsInterface
     {
-        $override = ($configProps !== null) ? $configProps : '\\Configs\\ConfigProps';
+        $override = ($configProps !== null) ? $configProps : '\\Configs\\CliOptions';
         $default  = \MaplePHP\Unitary\Config\ConfigProps::class;
         $name = (class_exists($override)) ? $override : $default;
         if (!is_subclass_of($name, ConfigPropsInterface::class)) {
